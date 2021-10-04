6d324db4067722f141218abd6fa0dd41e3a950f6
Shaun Reddiar <shaun.reddiar@absa.africa>
2021-10-04 13:15:12 +0200

    Added sass to project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
+import './App.scss';
 import Header from './header/header';
 import ProjectDetails from './project-details/project-details';
+import ProjectLanes from './project-lanes/project-lanes';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="d-flex flex-column h-100">
+    <div className="d-flex flex-column h-100 vstack">
       <Header />
       <Container className="h-100" fluid>
         <Row className="h-100">
           <Col className="bg-secondary text-light p-3" md={2}>Menu</Col>
-          <Col className="p-3 d-flex">
+          <Col className="p-3 d-flex content-area">
             <ProjectDetails />
+            <ProjectLanes />
           </Col>
         </Row>
       </Container>
