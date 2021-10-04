ab269943da477c1db6a354cc6a78a00ce7931664
Shaun Reddiar <shaun.reddiar@absa.africa>
2021-10-04 09:33:48 +0200

    Added Project Detail Mark up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './header/header';
+import ProjectDetails from './project-details/project-details';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
       <Header />
       <Container className="h-100" fluid>
         <Row className="h-100">
-          <Col>Menu</Col>
-          <Col>Content</Col>
+          <Col className="bg-secondary text-light p-3" md={2}>Menu</Col>
+          <Col className="p-3 d-flex">
+            <ProjectDetails />
+          </Col>
         </Row>
       </Container>
     </div>
---
+++ src/project-details/project-details.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Row, Col, ProgressBar  } from 'react-bootstrap';
+
+const ProjectDetails = () => {
+    return (
+        <div className="project-details w-100">
+            <Row>
+                <Col>
+                    <div><strong>Date created:</strong><br/>21 March 2021</div>
+                    <div><strong>Created by:</strong><br/>Shaun Reddiar</div>
+                </Col>
+                <Col>
+                    <div><strong>Name:</strong><br/>Project 1</div>
+                    <div><strong>Description:</strong><br/>This is just a small description on the project</div>
+                </Col>
+                <Col>
+                    <div><strong>Due Date:</strong><br/>23 November 2021</div>
+                    <div><strong>Days Left:</strong><br/>90</div>
+                </Col>
+                <Col><div><strong>Participants:</strong><br/>Shaun Reddiar, John Doe, Jane Doe</div></Col>
+            </Row>
+            <Row className="mt-4">
+                <Col>
+                    <p><strong>Progress:</strong></p>
+                    <ProgressBar max="100" now="50" />
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default ProjectDetails;
